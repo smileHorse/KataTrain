76f4dcbf13c1fa4634b38f8a46bc6b8248543cf0
smileHorse <65022492@qq.com>
2018-06-06 08:51:38 +0800

    add new

---
+++ js/ConvertStringToCamelCase.js
@@ -0,0 +1,20 @@
+/**
+Complete the method/function so that it converts dash(短)/underscore(下划线) delimited(分割) words into camel casing. 
+The first word within the output should be capitalized only if the original word was capitalized.
+
+Examples
+
+toCamelCase("the-stealth-warrior") // returns "theStealthWarrior"
+
+toCamelCase("The_Stealth_Warrior") // returns "TheStealthWarrior"
+**/
+
+// my solution
+function toCamelCase(str){
+
+}
+
+// other solution
+function toCamelCase(str){
+
+}
